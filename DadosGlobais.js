738e35bab8cbf851d7ccf577dc2279429faab09c
vnkauaa <kaua.ev@escola.pr.gov.br>
2024-10-15 16:55:23 -0300

    15/10/24

--- DadosGlobais.js
+++ DadosGlobais.js
@@ -5,7 +5,17 @@ console.log(url);
 async function VizualizarInformacoesGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
-    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML= `Você Sabia que o mundo tem 
+    ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
+    ${dados.total_pessoas_conctadas} estão conectadas em alguma rede social e passam em media ${dados.tempo_medio}
+    horas conctadas`
+
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
+
+    console.log(paragrafo);
 }
 
 VizualizarInformacoesGlobais()
